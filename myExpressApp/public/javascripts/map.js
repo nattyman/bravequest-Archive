7375904f156dd2602553e5163ce932587bd59f13
Heath Howard <nattyman512@gmail.com>
2019-12-24 22:03:55 -0600

    Started map coordinates

---
+++ myExpressApp/public/javascripts/map.js
@@ -0,0 +1,43 @@
+var coordinates = {
+    1: [
+        {
+            "x":1,
+            "y":1,
+            "name": "Kingsport",
+            "description":"Kingsport is the capital city and is located in the far Northeast corner of the land in a bay. The city is filled with all types of shops",
+            "explore": "There is a food market, dry goods store, or you can see a way to the port"
+        },
+        {
+            "x":1,
+            "y":2,
+            "name": "Applewood Forest Road",
+            "description":"The road through Applewood forest",
+            "explore": "You are on a well maintained road surrounded light woods."
+        },
+        {
+            "x":1,
+            "y":3,
+            "name": "Applewood Forest Road",
+            "description": "The road through Applewood forest",
+            "explore": "You are on a well maintained road surrounded light woods."
+        }
+    2: [
+        {
+            "x":2,
+            "y":1,
+        }
+    ]
+]
+
+
+
+//JSON example
+myObj = {
+    "name":"John",
+    "age":30,
+    "cars": [
+      { "name":"Ford", "models":[ "Fiesta", "Focus", "Mustang" ] },
+      { "name":"BMW", "models":[ "320", "X3", "X5" ] },
+      { "name":"Fiat", "models":[ "500", "Panda" ] }
+    ]
+   }
