2b81f2725f529e759d8d2be813add2d5eac5e25a
Heath Howard <nattyman512@gmail.com>
2019-12-25 17:29:01 -0600

    Improved JSON format in map.js

--- myExpressApp/public/javascripts/map.js
+++ myExpressApp/public/javascripts/map.js
@@ -1,43 +1,82 @@
-var coordinates = {
-    1: [
-        {
-            "x":1,
-            "y":1,
-            "name": "Kingsport",
-            "description":"Kingsport is the capital city and is located in the far Northeast corner of the land in a bay. The city is filled with all types of shops",
-            "explore": "There is a food market, dry goods store, or you can see a way to the port"
-        },
-        {
-            "x":1,
-            "y":2,
-            "name": "Applewood Forest Road",
-            "description":"The road through Applewood forest",
-            "explore": "You are on a well maintained road surrounded light woods."
-        },
-        {
-            "x":1,
-            "y":3,
-            "name": "Applewood Forest Road",
-            "description": "The road through Applewood forest",
-            "explore": "You are on a well maintained road surrounded light woods."
-        }
-    2: [
-        {
-            "x":2,
-            "y":1,
-        }
-    ]
-]
+ location = {
+        "coordinates": [{
+                "x": 1,
+                "y": 1,
+                "location": {
+                    "Name": "Kingsport",
+                    "Transition": "the great city of Kingsport"
+                }
+            }, {
+                "x": 2,
+                "y": 1,
+                "location": {
+                    "Name": "Applewood Forest",
+                    "Transition": "the road through Applewood Forest"
+                }
+            },
+            {
+                "x": 3,
+                "y": 1,
+                "location": {
+                    "Name": "Applewood Forest",
+                    "Transition": "deeper into Applewood Forest"
+                }
+            }
+        ]
+    }
 
 
 
-//JSON example
-myObj = {
-    "name":"John",
-    "age":30,
-    "cars": [
-      { "name":"Ford", "models":[ "Fiesta", "Focus", "Mustang" ] },
-      { "name":"BMW", "models":[ "320", "X3", "X5" ] },
-      { "name":"Fiat", "models":[ "500", "Panda" ] }
-    ]
-   }
+
+
+
+// var coordinates = {
+//     1: [
+//         { 
+//             1:[ {
+//                 "x":1,
+//                 "y":1,
+//                 "name": "Kingsport",
+//                 "description":"Kingsport is the capital city and is located in the far Northeast corner of the land in a bay. The city is filled with all types of shops",
+//                 "transition":"You've entered Kingsport, the greatest city in the land."
+//                 "explore": "There is a food market, dry goods store, or you can see a way to the port"
+//             } ]
+//         },
+//         {
+//             2:[
+//                 "x":1,
+//                 "y":2,
+//                 "name": "Applewood Forest Road",
+//                 "description":"The road through Applewood forest",
+//                 "explore": "You are on a well maintained road surrounded by light woods."
+//             ]
+//         },
+//         {
+//             "x":1,
+//             "y":3,
+//             "name": "Applewood Forest Road",
+//             "description": "The road through Applewood forest",
+//             "explore": "The woods all look the same except you see a small deer trail to the South."
+//         }
+//     2: [
+//         {
+//             "x":2,
+//             "y":1,
+//         }
+//     ]
+// ]
+
+
+
+// //JSON example
+// myObj = {
+//     "name":"John",
+//     "age":30,
+//     "cars": [
+//       { "name":"Ford", "models":[ "Fiesta", "Focus", "Mustang" ] },
+//       { "name":"BMW", "models":[ "320", "X3", "X5" ] },
+//       { "name":"Fiat", "models":[ "500", "Panda" ] }
+//     ]
+//    }
+
+//    coordinates:[{x:1,y:1}]
