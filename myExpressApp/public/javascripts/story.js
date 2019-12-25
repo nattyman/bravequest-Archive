f088f68bdf6c68e54347b53c7c932b27ec959bfe
Heath Howard <nattyman512@gmail.com>
2019-12-24 20:59:20 -0600

    formatted main entry field

---
+++ myExpressApp/public/javascripts/story.js
@@ -0,0 +1,13 @@
+var intro1 = {
+    story:"William is a poor farmer. The kings daughter is kidnapped by the evil wizard Elixer and ransomed for the price of the entire kingdom.  The King puts out a reward for up-to half his kingdom and his daughters hand in marriage for whomever can defeat Elixer and return the princes safely.",
+    link:"intro2"
+};
+
+var intro2 = {
+    story: "William is a simple man and has no interest in reward or marrying a princes. After many brave Knights attempt to resque the princes but never return, it appears as if the king will have to hand over his good kingdom to the wicked wizard.  After returning from the market, William finds his home and farm completely destroyed  by the hand of Elixer's minions.",
+    link:"intro3"
+}
+var intro3 = {
+    story: "With nothing to lose and revenge in his heart, William takes his pitchfork and begins his brave quest to destroy the monster who took everything from him...",
+    link:"Begin"
+}
