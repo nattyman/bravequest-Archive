e932d6e7a4f6565cdeb80f1a8b8756c3e87cef8e
Heath Howard <nattyman512@gmail.com>
2019-12-26 21:43:45 -0600

    Refactoring, moved js from index to game.js

---
+++ myExpressApp/public/javascripts/game.js
@@ -0,0 +1,30 @@
+function printLocation() {
+    document.getElementById("myCoordinates ").innerHTML = "x:" + myCoordinates .x + " y:" + myCoordinates .y ;
+  }
+ 
+ function runGame() {
+ //- Print location
+    printLocation();
+    var alert = "";
+    myMap = map;
+    console.log(myMap.x[1].y[1].name);
+ }
+//- function to receive input and print it on the page
+function enterCommand() {
+  //- Prevent the form from reloading the page 
+  event.preventDefault(); 
+
+    //- Recieve the input into "newCommand"
+  let newCommand = document.getElementById("command").value;
+  //-
+  alert = "";
+  move(newCommand);
+  setLocation();
+  printLocation();
+  if (alert) {
+    newCommand += "<br><span style='color:red; font:bold'>" + alert + "</span>";
+  }
+  console.log(alert);
+  document.getElementById("story").innerHTML = newCommand; 
+}
+
